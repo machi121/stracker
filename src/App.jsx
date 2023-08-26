d969ac96dc384a3dbc9b68e793adce67f9db2831
Hamse <arwohamseu@gmail.com>
2023-08-26 12:40:11 -0400

    Added stats elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import "./App.css";
+
+
+
+import{
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import {Pie} from 'react-chartjs-2';
+
+
+ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend
+);
 
 
 function App() {
-  
+  const data = {
+    labels: ['Subject#1',"Subject#2","Subject#3"],
+    datasets:[{
+        // label:'Poll',
+        data: [3,6,8],
+        backgroundColor:["#A2678A","#102C57","#3D246C"],
+        borderColor:["black"],
+        Color:["white"]
+
+    }]
+  }
+  const options = {
+    responsive: true
+
+    
+
+  }
 
   return (
     <>
@@ -19,8 +53,23 @@ function App() {
 
     {/* Stats Code */}
 
-
-
+        <div className="stats-section">
+            <h1>User Stats</h1>        
+            <div className='stats'>
+                
+                <div className="pie-graph">
+                    <Pie 
+                    data = {data}
+                    options = {options}>
+                    </Pie>
+                </div>
+                <div className="other-stats">
+                    <h2>Total Hours: 102hr</h2>
+                    {/* <h2>Total Hours: 102hr</h2> */}
+                    <h2>Average per day 13.3hr/day</h2>
+                </div>
+            </div>
+        </div>
       </div>
     </>
   );
