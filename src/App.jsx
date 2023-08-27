4bf80945cc34994ec703bae50160197a9d23be36
Hamse <arwohamse@gmail.com>
2023-08-26 20:38:12 -0400

    Change the stat into a graph

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,41 @@
-import React, { useState } from 'react';
-import './App.css';
-import pfp from './images/ayanokoji.jpg';
-import{
-    Chart as ChartJS,
-    ArcElement,
-    Tooltip,
-    Legend
-} from 'chart.js'
-import {Pie} from 'react-chartjs-2';
+import React, { useState } from "react";
+import "./App.css";
+import pfp from "./images/ayanokoji.jpg";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Bar, Pie } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 
-ChartJS.register(
-    ArcElement,
-    Tooltip,
-    Legend
-);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const App = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
   const [profilePicture, setProfilePicture] = useState(pfp);
-  const [name, setName] = useState('綾小路 清隆');
-  const [username, setUsername] = useState('ayanokoji.kiyotaka');
-  const [gender, setGender] = useState('Male');
+  const [name, setName] = useState("綾小路 清隆");
+  const [username, setUsername] = useState("ayanokoji.kiyotaka");
+  const [gender, setGender] = useState("Male");
   const [age, setAge] = useState(17);
-  const [grade, setGrade] = useState('2年生');
-  const [school, setSchool] = useState('東京都高度育成高等学校');
-  const [location, setLocation] = useState('Tokyo, Japan');
-  const [bio, setBio] = useState('Everyone is just a tool to me.');
+  const [grade, setGrade] = useState("2年生");
+  const [school, setSchool] = useState("東京都高度育成高等学校");
+  const [location, setLocation] = useState("Tokyo, Japan");
+  const [bio, setBio] = useState("Everyone is just a tool to me.");
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
-  const lightColors = ['#FF7E67', '#66A4FF', '#6FCF97']; 
-  const darkColors = ['#A2678A', '#102C57', '#3D246C']; 
+  const lightColors = ["#96B6C5", "#A0C49D", "#A9907E"];
+  const darkColors = ["#614BC3", "#678983", "#352F44"];
 
   const data = {
-    labels: ['Subject#1', 'Subject#2', 'Subject#3'],
+    labels: ["Subject#1", "Subject#2", "Subject#3"],
     datasets: [
       {
+        label: "Poll",
         data: [3, 6, 8],
-        backgroundColor: theme === 'light' ? lightColors : darkColors, 
-        borderColor: ['black'],
-        color: ['white'],
+        backgroundColor: theme === "light" ? lightColors : darkColors,
+        borderColor: ["black"],
+        color: ["white"],
+        borderWidth: 1,
       },
     ],
   };
@@ -53,6 +47,19 @@ const App = () => {
         borderWidth: 0, // Set the border width to 0 to remove outlines
       },
     },
+  };
+
+  const total_data = {
+    labels: ["Total Hours", "Average Hours"],
+    datasets: [
+      {
+        label: "Poll",
+        data: [120.4, 93],
+        backgroundColor: theme === "light" ? lightColors : darkColors,
+        borderColor: ["black"],
+        color: ["white"],
+      },
+    ],
   };
 
   return (
@@ -70,7 +77,10 @@ const App = () => {
             <div className="name">{name}</div>
             <div className="username">@{username}</div>
             <div className="detail">
-              <i className="fas fa-user"></i> <span>{gender}, {age} years old</span>
+              <i className="fas fa-user"></i>{" "}
+              <span>
+                {gender}, {age} years old
+              </span>
             </div>
             <div className="detail">
               <i className="fas fa-graduation-cap"></i> <span>{grade}</span>
@@ -88,12 +98,11 @@ const App = () => {
         </div>
       </div>
 
-      
       {/* Stats Code */}
 
       <div className="stats-section">
         <h1>User Stats</h1>
-        <div className='stats'>
+        <div className="stats">
           <div className="pie-graph">
             <Pie
               data={data}
@@ -102,11 +111,10 @@ const App = () => {
             />
           </div>
           <div className="other-stats">
-            <h2>Total Hours: 102hr</h2>
-            <h2>Average per day 13.3hr/day</h2>
+            <Doughnut data={total_data} options={options} />
           </div>
         </div>
-      </div>  
+      </div>
     </div>
   );
 };
