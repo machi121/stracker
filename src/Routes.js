03ff5a166ba6a146815db3d6c57e2e458c947cbb
machi121 <110929254+machi121@users.noreply.github.com>
2023-08-27 16:59:03 -0400

    Set up login redirect

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,22 @@
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-import React, { Fragment } from "react";
 import MainPage from "./pages/mainPage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 export const Routez = () => {
+  // Redirect function
+  const redirectToLogin = () => {
+    window.location.href = "https://strack1.auth.ca-central-1.amazoncognito.com/login?client_id=gpm3id372kcq7o79l6roja1gc&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth-callback";
+  };
+
   return (
     <Router>
-      <Fragment>
-        <Routes>
-          <Route path="/auth-callback" element={<AuthCallbackPage />}></Route>
-          <Route path="/main" element={<MainPage />}></Route>
-          <Route
-            path="/login"
-            element={
-              <Navigate to="https://strack1.auth.ca-central-1.amazoncognito.com/login?client_id=gpm3id372kcq7o79l6roja1gc&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth-callback" />
-            }
-          />
-        </Routes>
-      </Fragment>
+      <Routes>
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/main" element={<MainPage />} />
+        {/* Redirect from root */}
+        <Route path="/" element={redirectToLogin} />
+      </Routes>
     </Router>
   );
 };
